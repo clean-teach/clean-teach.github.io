0efc007a452560be3be91be372a11cfa96f81256
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-18 03:43:35 +0900

    스타일 수정 및 기능 추가

--- js/greetings.js
+++ js/greetings.js
@@ -1,5 +1,8 @@
+const loginArea = document.querySelector("#login-area");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const btnLogOut = document.querySelector("#btn-logout");
+const wrapper = document.querySelector("#wrapper");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
@@ -7,22 +10,31 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginArea.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
+function handleLogOut() {
+  localStorage.removeItem(USERNAME_KEY);
+  loginArea.classList.remove(HIDDEN_CLASSNAME);
+  wrapper.classList.add(HIDDEN_CLASSNAME);
+}
 
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  wrapper.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginArea.classList.remove(HIDDEN_CLASSNAME);
+  wrapper.classList.add(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+  loginArea.classList.add(HIDDEN_CLASSNAME);
+  wrapper.classList.remove(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
+  btnLogOut.addEventListener('click', handleLogOut);
 }
